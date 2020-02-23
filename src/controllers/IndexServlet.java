26411cdb41d46e43cbf5e85ab03f07008634b607
hiromasa.magome <magome0813@gmail.com>
2020-02-24 00:45:19 +0900

    Add getAllTasks

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,7 +35,7 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> messages = em.createNamedQuery("getAllMessages", Task.class)
+        List<Task> messages = em.createNamedQuery("getAllTasks", Task.class)
                                    .getResultList();
 
         em.close();
